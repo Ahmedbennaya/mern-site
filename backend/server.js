e9af1ea6137f5564e617adf05760b3c2a52ea6e0
Ahmedbennaya <aopenking95@gmail.com>
2024-09-21 20:29:16 +0200

    Fix babel not found issue

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 // CORS configuration
-app.use(cors({ credentials: true, origin: "http://localhost:3000",}));
+app.use(cors({ credentials: true, origin: "http://localhost:3000",origin:"https://mern-task-app-ciz9.onrender.com"}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
