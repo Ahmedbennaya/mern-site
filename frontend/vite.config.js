03e59c67dabc855517aae49541ddb5cbb46f7a89
Ahmedbennaya <aopenking95@gmail.com>
2024-09-24 00:17:41 +0200

    Fix babel not found issue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,30 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// Vite configuration
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],                    // Include Vite React plugin
-  server: {
-    port: 3000,                          // Dev server port
-    open: true,                          // Auto open browser on server start
-    proxy: {
-      '/api': {                          // Proxy API requests
-        target: 'http://localhost:5000', // Backend server
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  },
-  build: {
-    chunkSizeWarningLimit: 600,          // Adjust chunk size limit to avoid warnings
-    rollupOptions: {
-      output: {
-        manualChunks(id) {               // Separate dependencies into chunks
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
-          }
-        }
-      }
-    }
+  plugins: [react()],
+  server:{
+    port:3000
   }
-});
+})
