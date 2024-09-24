1a9a9c565a3e2cd14f9803ea235178bd2d46c61e
Ahmedbennaya <aopenking95@gmail.com>
2024-09-24 03:09:07 +0200

    Fix babel not found issue

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 // CORS configuration
-app.use(cors({ credentials: true, origin: ["http://localhost:3000", "https://api.render.com/deploy/srv-cro5j6pu0jms73c70kp0?key=2c7tfN5hx2E"] }));
+app.use(cors({ credentials: true, origin: ["http://localhost:3000", "https://66f20e08135ee444da2d3776--bargaoui.netlify.app/"] }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
