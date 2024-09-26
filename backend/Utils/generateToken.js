cdf44aafa1e0f33a95aa717328219fc5e4389a01
Ahmedbennaya <aopenking95@gmail.com>
2024-09-26 22:43:09 +0200

    Fix babel not found issue

--- backend/Utils/generateToken.js
+++ backend/Utils/generateToken.js
@@ -7,8 +7,8 @@ const generateToken = (res, userId) => {
 
   res.cookie("JWT", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production", // Ensures secure cookies in production (HTTPS only)
-    sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax", // Allows cookies for cross-domain requests
+    secure: process.env.NODE_ENV === "development", // Ensures secure cookies in production (HTTPS only)
+    sameSite: process.env.NODE_ENV === "development" ? "None" : "Lax", // Allows cookies for cross-domain requests
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   });
 };
