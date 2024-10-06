9343e2ae70cc837d84da52b64dc06937b43129f5
Ahmedbennaya <aopenking95@gmail.com>
2024-10-06 23:37:37 +0200

    Fix babel not found issue

--- backend/server.js
+++ backend/server.js
@@ -27,9 +27,7 @@ const PORT = process.env.PORT || 4000;
 app.use(cors({
   credentials: true,
   origin: [
-    "https://mern-site-z5gs.onrender.com",
-    "https://main--bargaoui.netlify.app",
-    "http://localhost:3000"
+    "https://mern-site-z5gs.onrender.com","http://localhost:3000"
   ]
 }));
 
